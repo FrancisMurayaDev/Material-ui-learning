8cee5c7e429a6e822aa31d07fd57060a19847c12
FrancisMurayaDev <francismurayadev@gmail.com>
2025-04-09 21:30:12 +0300

    update notes

--- README.md
+++ README.md
@@ -19,3 +19,39 @@ Default Installation
 ```js
 npm install @mui/material @emotion/react @emotion/styled
 ```
+
+## Importing Typography and setting the variant.
+
+```js
+import {Typography} from "@mui/material"
+
+function App () {
+  return (
+    <>
+      <Typography variant="h1">Material UI</Typography>
+    </>
+  )
+}
+
+export default App;
+```
+
+
+- Typography deals with style and appearace of text in a website.
+
+It takes a variant prop. 
+
+#### Variant Prop:
+
+There are upto 13 variants::
+
+1. h1 to h6
+2. body1, body2 - used for a pragragraph.
+3. subtitle1, subtitle2 - they also render a h6.
+4. button
+5. caption
+6. overline
+
+Component prop can be used with variant prop where we want to keep the variant styling. 
+
+
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import {Typography} from "@mui/material"
+
 function App () {
   return (
-    <div>
-      <h1>Material UI</h1>
-    </div>
+    <>
+      <Typography variant="h1" component="h3">Heading 1</Typography>
+      <Typography variant="h2">Heading 2</Typography>
+      <Typography variant="h3">Heading 3</Typography>
+      <Typography variant="h4">Heading 4</Typography>
+      <Typography variant="h5">Heading 5</Typography>
+      <Typography variant="h6">Heading 6</Typography>
+      <Typography variant="subtitle1">Subtitle1</Typography>
+      <Typography variant="subtitle2">Subtitle2</Typography>
+      <Typography variant="body1">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat assumenda numquam similique dicta cupiditate animi quos accusantium saepe aut pariatur, inventore nesciunt consequatur impedit sunt tenetur sit corporis? Aspernatur deleniti ullam ducimus. Iure, ipsam modi! Accusantium itaque natus et, aliquam obcaecati quasi assumenda nobis, sed architecto porro quam. Cumque modi, dolorem eum recusandae necessitatibus at delectus quod sequi, aliquid fugiat aut cum quibusdam ex exercitationem assumenda ad nisi nesciunt quo placeat dolor temporibus sunt iure quisquam. Repellat quibusdam repudiandae, et id aliquam labore aut, in fuga reprehenderit sint illum asperiores quas cumque dolores impedit numquam tempora consequatur veritatis vel pariatur.
+      </Typography>
+
+      <Typography variant="body2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae cumque nulla ex! Recusandae, facere qui. Laborum magni odio esse! Explicabo temporibus culpa, eius officia pariatur mollitia, autem facere voluptatibus nulla tempore praesentium. Eligendi, ab enim! Molestiae fuga error sequi unde blanditiis possimus eaque placeat aperiam sit! Reiciendis vitae laborum commodi?</Typography>
+    </>
   )
 }
 
